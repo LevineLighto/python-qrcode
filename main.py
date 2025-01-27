4fa2c84ab205d7964e287e6c392a3c255836c388
LevineLighto <hello@irimold.web.id>
2025-01-27 15:24:06 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import click
+import qrcode
+import qrcode.constants
+from drawers.base import CustomStyledPilImage
+from drawers.eyedrawers import CircleEyeDrawer, BarsEyeDrawer
+from drawers.moduledrawers import GappedCircleModuleDrawer
+from qrcode.image.styles.moduledrawers.pil import SquareModuleDrawer, GappedSquareModuleDrawer, RoundedModuleDrawer, VerticalBarsDrawer, HorizontalBarsDrawer
+
+@click.command()
+
+@click.argument('content')
+@click.argument('name')
+
+@click.option('--style', '-s')
+
+def main(content, name, style):
+    match style:
+        case "pixel":
+            module_drawer   = GappedSquareModuleDrawer(size_ratio = .75)
+            eye_drawer      = SquareModuleDrawer()
+        case "circle":
+            module_drawer   = GappedCircleModuleDrawer(size_ratio = .75)
+            eye_drawer      = CircleEyeDrawer()
+        case "rounded":
+            module_drawer   = RoundedModuleDrawer()
+            eye_drawer      = RoundedModuleDrawer()
+        case "v-bars":
+            module_drawer   = VerticalBarsDrawer(horizontal_shrink = .75)
+            eye_drawer      = BarsEyeDrawer()
+        case "h-bars":
+            module_drawer   = HorizontalBarsDrawer(vertical_shrink = .75)
+            eye_drawer      = BarsEyeDrawer()
+        case _:
+            module_drawer   = SquareModuleDrawer()
+            eye_drawer      = SquareModuleDrawer()
+
+    qr = qrcode.QRCode(
+        version             = None,
+        error_correction    = qrcode.constants.ERROR_CORRECT_H,
+        box_size            = 100,
+        border              = 10
+    )
+
+    qr.add_data(content)
+    qr.make(fit = True)
+    img = qr.make_image(
+        image_factory   = CustomStyledPilImage,
+        module_drawer   = module_drawer,
+        eye_drawer      = eye_drawer,
+        fill_color      = "black", 
+        back_color      = "white"
+    )
+    img.save("{}.png".format(name))
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,23 @@
+# QR Code Generator
+A CLI tool to generate QR Code
+
+## Installation
+Make sure that you have make installed on your pc
+```bash
+make install
+```
+
+## Usage
+```bash
+py main.py <your-data> <output-file-name> --style <rounded|pixel|circle|v-bars|h-bars>
+```
+
+Example:
+| Command | Output |
+| ------- | ------ |
+|`py main.py "Lorem ipsum dolor sit amet" "sample-square" ` | <img src="./docs/sample-square.png" width="160"/> |
+|`py main.py "Lorem ipsum dolor sit amet" "sample-rounded"  --style rounded` | <img src="./docs/sample-rounded.png" width="160"/> |
+|`py main.py "Lorem ipsum dolor sit amet" "sample-pixel"  --style pixel` | <img src="./docs/sample-pixel.png" width="160"/> |
+|`py main.py "Lorem ipsum dolor sit amet" "sample-circle"  --style circle` | <img src="./docs/sample-circle.png" width="160"/> |
+|`py main.py "Lorem ipsum dolor sit amet" "sample-v-bars"  --style v-bars` | <img src="./docs/sample-v-bars.png" width="160"/> |
+|`py main.py "Lorem ipsum dolor sit amet" "sample-h-bars"  --style h-bars` | <img src="./docs/sample-h-bars.png" width="160"/> |
